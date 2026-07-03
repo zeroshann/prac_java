f83a60d826f1f6a0d6c32cbad2dcac41cdc00868
zeroshann <carroshine153@gmail.com>
2026-07-04 07:51:08 +0800

    Add area_of_rectangle class with main method to calculate rectangle area

---
+++ src/area_of_rectangle.java
@@ -0,0 +1,14 @@
+public class area_of_rectangle {
+    
+    public static void main(String[] args) {
+        int length = 5;
+        int width = 6;
+        int area = length * width; //calculate area of rectangle
+        
+        //print the variables
+        System.out.println("Length of rectangle: " + length);
+        System.out.println("Width of rectangle: " + width);
+        System.out.println("Area of rectangle is: " + area);
+
+    }
+}
