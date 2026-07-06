ad33a10d0e119093bb2b9f15c31a891d5c6fba9f
zeroshann <carroshine153@gmail.com>
2026-07-06 11:40:29 +0800

    Add bools and sample classes to demonstrate boolean variables and data type usage

---
+++ src/bools.java
@@ -0,0 +1,10 @@
+public class bools {
+    
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        System.out.println("The value of isTrue is: " + isTrue); //output is true
+        System.out.println("The value of isFalse is: " + isFalse); //output is false
+    }
+}
---
+++ src/sample.java
@@ -0,0 +1,16 @@
+public class sample {
+
+    public static void main(String[] args) {
+        //crate vaariables of differernt data types
+        int items =  50;
+        float costPerItem = 9.99f;
+        float TotalCost = items * costPerItem;
+        char currency = '$';
+
+        //Print Variables
+        System.out.println("Number of items: " + items);
+        System.out.println("Cost per item: " + currency + costPerItem);
+        System.out.println("Total cost: " + currency + TotalCost);
+    }
+    
+}
