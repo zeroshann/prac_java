95323a80a0cbc07b7572eb690fd9ef1d07748f4b
zeroshann <carroshine153@gmail.com>
2026-07-07 15:37:03 +0800

    Add method class to demonstrate string manipulation and case conversion

---
+++ src/method.java
@@ -0,0 +1,10 @@
+public class method {
+       public static void main(String[] args) {
+        String txt = "sbdbwfnkwdaohHSDVHdiwnfjweiw";
+
+        System.out.println("The length of the string is: " + txt.length());
+        System.out.println("The string in uppercase is: " + txt.toUpperCase());
+        System.out.println("The string in lower case is: " + txt.toLowerCase());
+    }
+
+}
