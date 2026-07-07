40de7ba0de3e74143098d78884b671a5eb1b09e5
zeroshann <carroshine153@gmail.com>
2026-07-07 15:36:29 +0800

    Add finding class to demonstrate string indexOf method usage

---
+++ src/finding.java
@@ -0,0 +1,8 @@
+public class finding {
+    
+    public static void main(String[] args) {
+        String txt = "please locate where 'locate' occurs!";
+        System.out.println(txt.indexOf("where"));
+        System.out.println(txt.indexOf("a"));
+    }
+}
