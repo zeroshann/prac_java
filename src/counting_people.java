1632452d8ee9837ad88a6e6c3104545f3a55278e
zeroshann <carroshine153@gmail.com>
2026-07-07 15:36:15 +0800

    Add counting_people class to demonstrate basic increment and decrement operations

---
+++ src/counting_people.java
@@ -0,0 +1,17 @@
+public class counting_people {
+    
+    public static void main(String[] args) {
+        // Initialize the number of people
+        int people = 0;
+
+        people++;
+        people++;
+        people++;
+
+        System.out.println("Number of people: " + people); // Outputs 3
+
+        people--;
+        System.out.println("Number of people after one leaves: " + people); // Outputs
+
+    }
+}
