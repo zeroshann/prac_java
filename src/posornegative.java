7ad687a35217957577f794e181ed874b18eeacf8
zeroshann <carroshine153@gmail.com>
2026-07-11 00:35:54 +0800

    Add posornegative class to determine number positivity

---
+++ src/posornegative.java
@@ -0,0 +1,16 @@
+public class posornegative {
+    
+    public static void main(String[] args) {
+
+        int myNum = 10;
+
+        if (myNum > 0){
+            System.out.println("The value is a positive number.");
+        } else if (myNum < 0){
+            System.out.println("The value is a negative number.");
+        } else{
+            System.out.println("The value is zero.");
+        }
+
+    }
+}
