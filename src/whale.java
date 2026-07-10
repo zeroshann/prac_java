03c39d65bc7da4cc735c5fbc7d96a84a913d3cea
zeroshann <carroshine153@gmail.com>
2026-07-11 00:36:25 +0800

    Add whale class to implement countdown and New Year message

---
+++ src/whale.java
@@ -0,0 +1,13 @@
+public class whale {
+    
+    public static void main(String[] args) {
+        int countdown = 3;
+
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+        System.out.println("Happy New Year!");
+    }
+}
