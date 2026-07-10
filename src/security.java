c8ace095bd95b9df654b785c021ff286adb27b68
zeroshann <carroshine153@gmail.com>
2026-07-10 11:22:26 +0800

    Add binary file for security class

---
+++ src/security.java
@@ -0,0 +1,13 @@
+public class security {
+    
+    public static void main(String[] args) {
+
+        int doorCode = 1137;
+
+        if (doorCode == 1137) {
+            System.out.println("Correct code. Access granted.");
+        } else {
+            System.out.println("Incorrect code. Access denied.");
+        }
+    }
+}
