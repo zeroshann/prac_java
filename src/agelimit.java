53242eda4a198d2ba59f8893309a0557b287696a
zeroshann <carroshine153@gmail.com>
2026-07-11 00:35:17 +0800

    Add binary file for agelimit class

---
+++ src/agelimit.java
@@ -0,0 +1,16 @@
+public class agelimit {
+    
+    public static void main(String[] args) {
+
+        int myAge = 25;
+        int votingAge = 18;
+
+        if (myAge >= votingAge) {
+            System.out.println("old enough to vote.");
+        } else {
+            System.out.println("not old enough to vote.");
+        }
+
+
+    }
+}
