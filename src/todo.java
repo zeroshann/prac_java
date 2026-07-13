50eaa2138e68341d19434fb2f788515cf87b4471
zeroshann <carroshine153@gmail.com>
2026-07-13 20:46:39 +0800

    Add todo class with main method and simple loop

---
+++ src/todo.java
@@ -0,0 +1,11 @@
+public class todo {
+    
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("i is " + i);
+            i++;
+        }
+        while (i < 5);
+    }
+}
